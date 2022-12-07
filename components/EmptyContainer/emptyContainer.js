01691e6f72d34326c8cb4f8a8b01afb5df1029e6
Martin <mdsugarev@gmail.com>
2022-12-07 09:38:15 +0200

    refact

---
+++ components/EmptyContainer/emptyContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function EmptyContainer({search, clearInput}){
+    return (
+        <div className="empty-container">
+            <p>No results found for "{search}". <b>How about some burgers?</b></p>
+            <button onClick={() => clearInput()} className="empty-button">Yeah, sure</button>
+        </div>
+    )
+}
